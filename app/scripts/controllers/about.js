5890f264fd5f4f8ed06f8b155618ae8344e3f11d
Devgaze <alen@devgaze.com>
2016-05-22 13:04:48 +0100

    replaced deprecated `autoprefixer-core` with favoured `autorpefixer`; updated `grunt-postcss` with newer version and re-referenced `autoprefixer-core` to `autoprefixer`; removed `grunt-angular-templates`

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bapApp
+ */
+angular.module('bapApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
