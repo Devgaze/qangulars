b17997bfcf27ab86d94e93f6655d2c6146bd1eac
Devgaze <alen@devgaze.com>
2016-05-22 13:14:07 +0100

    added `karma-spec-reporter` for more descriptive output

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -60,8 +60,19 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-spec-reporter'
     ],
+
+    reporters: ['spec'],
+    specReporter: {
+        maxLogLines: 5,         // limit number of lines logged per test
+        suppressErrorSummary: true,  // do not print error summary
+        suppressFailed: false,  // do not print information about failed tests
+        suppressPassed: false,  // do not print information about passed tests
+        suppressSkipped: true,  // do not print information about skipped tests
+        showSpecTiming: false // print the time elapsed for each spec
+    },
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
